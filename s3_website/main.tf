c86f6513915f5ee35578abb011f694f8c3f7e69f
Lodr222 <35805858+Lodr222@users.noreply.github.com>
2024-06-14 11:07:22 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -10,6 +10,8 @@ terraform {
 # Configure AWS provider and creds
 provider "aws" {
 	region = "us-east-1"
+	shared_credentials_files = ["~/.aws/credentials"]
+	profile = "default"
 }
 
 # Creating bucket
