ecff928d39feee7c002059b614ec6482d9e0a029
Lodr222 <35805858+Lodr222@users.noreply.github.com>
2024-06-14 11:40:36 +0300

    Update main.tf

--- s3_website/main.tf
+++ s3_website/main.tf
@@ -10,6 +10,7 @@ terraform {
 # Configure AWS provider and creds
 provider "aws" {
 	region = "us-east-1"
+	shared_config_files = ["./config"]
 	shared_credentials_files = ["./credentials"]
 	profile = "default"
 }
